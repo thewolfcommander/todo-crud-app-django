ecaa47f06dfd9d952e9f401ccc07bb51166adc74
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-03-03 16:08:51 +0530

    New Changes Updated

---
+++ core/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.views import auth_login
+
+from core import forms
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            form = forms.SignUpForm(request.POST or None)
+            if form.is_valid():
+                form.save()
+                auth_login(request)
+                return redirect('home')
+        else:
+            form = forms.SignUpForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'signup.html', context)
